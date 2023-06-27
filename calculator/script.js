dcfbaf48e06560fe164d3af2422a0e5f88ca0bf6
Manoj Rontija <78212463+manojmager@users.noreply.github.com>
2023-06-26 22:08:06 -0400

    Commit upload

---
+++ calculator/script.js
@@ -0,0 +1,16 @@
+// This function clear all the values
+function clearScreen() {
+    var val = document.getElementById("result").value = "0";
+}
+ 
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns result
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
